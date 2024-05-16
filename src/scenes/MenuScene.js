64ff9a87b459950ea03efd26ffb0b93724553676
“Nicolas <ncorfmat@ucsc.edu>
2024-05-16 10:01:23 -0700

    updated menu animation

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -13,7 +13,7 @@ class MenuScene extends BaseScene {
   create() {
     super.create();
     this.createMenu(this.menu, this.setUpMenuEvents.bind(this)); // second argument will bind the correct "this" context
-    this.createCharacter()
+    this.createCharacter();
   }
 
   setUpMenuEvents(menuItem) {
@@ -40,48 +40,33 @@ class MenuScene extends BaseScene {
     });
   }
 
+  update() {
+    this.playerCrying.body.setAccelerationX(this.playerCrying.x < game.config.width/2 ? 50 : -50);
+    this.playerCrying.body.setAccelerationY(this.playerCrying.y >= game.config.height/2 ? -50 : 50);
+
+    this.playerScared.body.setAccelerationX(this.playerScared.x < game.config.width/2 ? 50 : -50);
+    this.playerScared.body.setAccelerationY(this.playerScared.y >= game.config.height/2 ? -50 : 50);
+
+    this.playerAttack.body.setAccelerationX(this.playerAttack.x < game.config.width/2 ? 50 : -50);
+    this.playerAttack.body.setAccelerationY(this.playerAttack.y >= game.config.height/2 ? -50 : 50);
+  }
+
   createCharacter() {
     createAnimations(this.anims);
-    this.player = this.add.sprite(-50, game.config.height/2 + 200, "player")//.setScale(2)
-    this.player.play("crying")
+    
+    this.playerCrying = this.physics.add
+    .sprite(200, 200, "player")
+    .setScale(3)
+    .play("crying");
 
-    let tweenChain = this.tweens.chain({
-      targets: this.player,
-      loop: -1,
-      tweens: [
-          {
-              x: 150,
-              y: game.config.height/2,
-              duration: 1500,
-              scale: { from: 2, to: 4 },
-              angle: { from: 0, to: 90 },
-              ease: 'Quadratic.easeInOut'
-          },
-          {
-              x: game.config.width/2,
-              y: 150,
-              duration: 1500,
-              scale: { from: 4, to: 2 },
-              angle: { from: 90, to: 180 },
-              ease: 'Quadratic.easeInOut'
-          },
-          {
-            x: game.config.width - 150,
-            y: game.config.height/2,
-            duration: 1500,
-            scale: { from: 2, to: 4 },
-            angle: { from: 180, to: 270 },
-            ease: 'Quadratic.easeInOut'
-          },
-          {
-              x: game.config.width/2,
-              y: game.config.height - 150,
-              duration: 1500,
-              scale: { from: 4, to: 2 },
-              angle: { from: 270, to: 360 },
-              ease: 'Quadratic.easeInOut'
-          }
-      ]
-  })
+    this.playerScared = this.physics.add
+    .sprite(game.config.width - 200, game.config.height - 200, "player")
+    .setScale(3)
+    .play("scared");
+
+    this.playerAttack = this.physics.add
+    .sprite(200, game.config.height/2 + 100, "player")
+    .setScale(3)
+    .play("sword");
   }
 }
